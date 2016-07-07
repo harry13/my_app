563463a6b0a7b24ad79a913a719bb3fdc781067b
hariya.ryohei <hariya.ryohei@dena.jp>
2016-07-07 19:03:01 +0900

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
+// requireで読み込むときのrootのpathを指定できる。配列で複数の指定が可能。
 var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
   devtool: 'eval',
-  context: __dirname + '/src',
+  // entryオプションを解決する為のベースとなるディレクトリ
+  context: __dirname + '/public',
   entry: {
-    public: './index'
+    // 下の output.filename で使用されている[name]には、"public" がセットされます。
+    public: './src/index'
   },
   output: {
     path: path.join(__dirname, 'public/dist'),
     filename: 'bundle.js',
+    // publicPath は webpack-dev-server で自動コンパイルするために必要（URLにおけるJSファイルへのパスを書く）
     publicPath: 'public/dist'
   },
   plugins: [],
